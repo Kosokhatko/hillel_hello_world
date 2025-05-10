590cdd6291015740ae3448217dfdc7aa436216ba
Kosokhatko <sashko2007@gmail.com>
2025-05-10 11:07:46 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ second_hillel_1st_assignment.py
@@ -0,0 +1,29 @@
+# Другий урок
+# the first assignment the first mod
+number: int = 1357
+
+num_1 = number // 1000
+num_2 = (number // 100) % 10
+num_3 = (number // 10) % 10
+num_4 = number % 10
+
+print(num_1)
+print(num_2)
+print(num_4)
+print(num_4)
+
+print("\n")
+
+# the first assignment the second mod
+number = int(input("Напишіть Ваше власне 4-изначне число: "))
+
+num_1 = number // 1000
+num_2 = (number // 100) % 10
+num_3 = (number // 10) % 10
+num_4 = number % 10
+
+print(num_1)
+print(num_2)
+print(num_4)
+print(num_4)
+
