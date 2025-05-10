cbef71272e43fdcd3445dc5c310fbf7b17ea7bfa
Kosokhatko <sashko2007@gmail.com>
2025-05-10 10:56:58 +0300

    Merge remote-tracking branch 'origin/master'

--- first_hillel.py
+++ first_hillel.py
@@ -3,8 +3,23 @@
 # help(print)
 
 # Другий урок
-# the first assignment
+# the first assignment the first mod
 number: int = 13578
+
+num_1 = number // 1000
+num_2 = (number // 100) % 10
+num_3 = (number // 10) % 10
+num_4 = number % 10
+
+print(num_1)
+print(num_2)
+print(num_4)
+print(num_4)
+
+print("\n")
+
+# the first assignment the second mod
+number = int(input("Напишіть Ваше власне 5-тизначне число: "))
 
 num_1 = number // 1000
 num_2 = (number // 100) % 10
@@ -32,6 +47,23 @@ digit5 = number % 10
 reversed_number = digit5 * 10000 + digit4 * 1000 + digit3 * 100 + digit2 * 10 + digit1
 
 print(reversed_number)
+
+print("\n")
+
+# the second assignment the second mod
+number = int(input("Напишіть Ваше власне 5-тизначне число: "))
+
+digit1 = number // 10000
+digit2 = (number // 1000) % 10
+digit3 = (number // 100) % 10
+digit4 = (number // 10) % 10
+digit5 = number % 10
+
+reversed_number = digit5 * 10000 + digit4 * 1000 + digit3 * 100 + digit2 * 10 + digit1
+
+print(reversed_number)
+
+
 
 
 
