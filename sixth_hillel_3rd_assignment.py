7d33d1550aaa64246426ff79805ee73aff5010fe
Kosokhatko <sashko2007@gmail.com>
2025-05-24 12:49:39 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	fifth_hillel_1st_assignment.py

---
+++ sixth_hillel_3rd_assignment.py
@@ -0,0 +1,9 @@
+def multiply_digits(number):
+    while number > 9:
+        product = 1
+        while number > 0:
+            digit = number % 10
+            product *= digit
+            number //= 10
+        number = product
+    return number
